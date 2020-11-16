be5601ac38370a0d0aef3d86a42b6eac73e62a41
mohammad salah ali <MsMainAcc0unt@gmail.com>
2020-11-16 15:25:49 +0200

    ch4

---
+++ pages/tests.py
@@ -0,0 +1,32 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from .views import HomePageView
+
+class HomePageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    # test if home page view exits
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    # check that view uses the correct template
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
+    
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'Homepage')
+    
+    def test_homepage_dose_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page')
+    
+    # check if path resolves to view function
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
