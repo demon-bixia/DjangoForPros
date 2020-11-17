65c2f519afc9435e8b70ee9ad633e8a49a404f2f
mohammad salah ali <MsMainAcc0unt@gmail.com>
2020-11-17 09:05:16 +0200

    ch6

--- books/settings.py
+++ books/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd party
+    'crispy_forms',
+
+    # local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
@@ -124,8 +129,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 
 AUTH_USER_MODEL="users.CustomUser"
 
 LOGIN_REDIRECT_URL="home"
 LOGOUT_REDIRECT_URL="home"
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import HomePageView, AboutView
 
 class HomePageTests(SimpleTestCase):
 
@@ -29,4 +29,33 @@ class HomePageTests(SimpleTestCase):
         self.assertEqual(
             view.func.__name__,
             HomePageView.as_view().__name__
+        )
+
+
+class AboutPageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    # test if home page view exits
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    # check that view uses the correct template
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+    
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
+    
+    def test_homepage_dose_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page')
+    
+    # check if path resolves to view function
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutView.as_view().__name__
         )
